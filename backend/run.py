4e69b72db39e9df64937538c87db3a4310639afb
Adam Golan <agolan@grupawp.pl>
2019-01-13 17:49:57 +0100

    preprocess symbols for cleaner keys

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template, Response, request, jsonify
+from flask_cors import CORS
 import requests
 
 api_key = '8g327gr4b3t9137t4b'
 app = Flask(__name__, template_folder='../front/build', static_folder='../front/build/static', static_url_path='/static')
+CORS(app)
+
+def symbols_mapper(symbol_obj):
+    result = {}
+    for key in symbol_obj:
+        new_key = key.split()[-1]
+        result[new_key] = symbol_obj[key]
+    return result
 
 @app.route('/')
 def index():
@@ -14,10 +23,9 @@ def symbols_autocomplete():
         return 'Bad request'
 
     symbol = request.args['symbol']
-    return Response(
-        requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={symbol}&apikey={api_key}'),
-        mimetype='application/json'
-    )
+    symbols = requests.get(f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={symbol}&apikey={api_key}').json()
+    symbols['bestMatches'] = [symbols_mapper(symbol) for symbol in symbols['bestMatches']]
+    return jsonify(symbols)
 
 @app.route('/quote')
 def last_day_quote():
