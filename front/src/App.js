351971ea6f712f42fd07762fe5a1883a76c37e17
Adam Golan <agolan@grupawp.pl>
2019-01-13 17:19:17 +0100

    frontend app setup

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grid>
+        <Row>
+          <Col xs={10} xsoffset={1}>
+            <input className="symbol-input" placeholder="Company symbol" />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
